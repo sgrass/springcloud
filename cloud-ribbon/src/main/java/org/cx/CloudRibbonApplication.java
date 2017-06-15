727b43a03cdf7bb7124e2e607798d089aabee0ce
xgrass <xgrass@foxmail.com>
2017-06-15 15:39:39 +0800

    HystrixDashboard仪表盘

--- cloud-ribbon/src/main/java/org/cx/CloudRibbonApplication.java
+++ cloud-ribbon/src/main/java/org/cx/CloudRibbonApplication.java
@@ -5,12 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * ribbon断路由添加注解 EnableHystrix
+ * hytrix的监控 EnableHystrixDashboard
+ * @author grass
+ *
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
+@EnableHystrixDashboard
 public class CloudRibbonApplication {
 
 //	@Bean
