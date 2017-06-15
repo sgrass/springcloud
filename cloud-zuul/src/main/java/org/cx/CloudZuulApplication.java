4d9ec42708afd5ffc98920640999fe529bd2cc24
xgrass <xgrass@foxmail.com>
2017-06-15 17:24:05 +0800

    zuul 路由和过滤

---
+++ cloud-zuul/src/main/java/org/cx/CloudZuulApplication.java
@@ -0,0 +1,20 @@
+package org.cx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author grass
+ *
+ */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class CloudZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudZuulApplication.class, args);
+	}
+}
