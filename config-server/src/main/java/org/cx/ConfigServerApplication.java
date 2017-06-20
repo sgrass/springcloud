5727ef456cb60e7abe00a7e41c2aa2765bc6abd8
xgrass <xgrass@foxmail.com>
2017-06-20 15:45:44 +0800

    config server和client注册到微服务

--- config-server/src/main/java/org/cx/ConfigServerApplication.java
+++ config-server/src/main/java/org/cx/ConfigServerApplication.java
@@ -3,14 +3,17 @@ package org.cx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * EnableConfigServer注解开启配置服务器
+ * EnableEurekaClient 注册微服务
  * @author grass
  *
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
