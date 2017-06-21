e41330492103d3c47c02da0679083f4e7ba6a920
xgrass <xgrass@foxmail.com>
2017-06-21 14:28:49 +0800

    添加rabbit，动态改变配置文件

--- config-client/src/main/java/org/cx/web/ConfigClientController.java
+++ config-client/src/main/java/org/cx/web/ConfigClientController.java
@@ -1,16 +1,19 @@
 package org.cx.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
 	@Value("${foo}")
 	private String foo;
 	
-	@RequestMapping(value="/getConfig")
+	@RequestMapping(value="/getConfig",method=RequestMethod.GET)
 	public String getConfig() {
 		return 	foo;
 	}
